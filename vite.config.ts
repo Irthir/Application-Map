c0e942ffdc8987a43133e524d3f2a62f876c26a8
Irthir <romain.schlotter@gmail.com>
2025-04-02 21:10:48 +0200

    Premier push

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Application-Map/', // Remplace 'nom-du-repository' par le nom de ton dépôt GitHub
 })
