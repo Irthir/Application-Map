2bf99c77f3cb9e3e548a534be193d55d0230192b
Irthir <romain.schlotter@gmail.com>
2025-04-23 06:49:37 +0200

    Correction connexion au serveur js

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Application-Map/', // <-- important pour GitHub Pages
   plugins: [react()],
-  base: '/Application-Map/', // Remplace 'nom-du-repository' par le nom de ton dépôt GitHub
 })
